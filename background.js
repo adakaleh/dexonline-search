e1828b8e8e1b9d5b693d8d4f29aca88f6fa3d26e
AdaKaleh <31895292+adakaleh@users.noreply.github.com>
2020-03-03 17:39:38 +0000

    avoid <all_urls> permission

--- background.js
+++ background.js
@@ -8,19 +8,25 @@ browser.contextMenus.create({
 // Alt+Shift+D
 browser.commands.onCommand.addListener(function(command){
 	if (command === 'cauta') {
-		// get current tab
-		browser.tabs.query({
-			currentWindow: true,
-			active: true
-		}).then((tabs) => {
-			// ask it for the selected text
-			browser.tabs.sendMessage(
-				tabs[0].id,
-				'selectionText'
-			).then(selectionText => {
-				if (selectionText)
-					browser.tabs.create({ url: 'https://dexonline.ro/definitie/' + selectionText });
+		// add listener to current tab
+		browser.tabs.executeScript({
+			file: "/content.js",
+			allFrames: true // TODO: doesn't actually work in iframes
+		}).then(result => {
+			// get current tab
+			browser.tabs.query({
+				currentWindow: true,
+				active: true
+			}).then((tabs) => {
+				// ask it for the selected text
+				browser.tabs.sendMessage(
+					tabs[0].id,
+					'selectionText'
+				).then(selectionText => {
+					if (selectionText)
+						browser.tabs.create({ url: 'https://dexonline.ro/definitie/' + selectionText });
+				});
 			});
-		});
+		})
 	}
 });
